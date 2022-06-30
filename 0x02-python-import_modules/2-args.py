f37345c422ce63547829a5db694297e96a9b2ea8
Nobby-code <norbertguda0@gmail.com>
2022-06-30 15:34:43 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
         print("{:d} argument.".format(arg_length))
     elif arg_length == 1:
         print("{:d} argument:".format(arg_length))
-        print("{:d}: {:s}".format(i, argv[i]))
+        print("{:d}: {:s}".format(i, argv[1]))
     else:
         print("{:d} arguments:".format(arg_length))
         while i <= arg_length:

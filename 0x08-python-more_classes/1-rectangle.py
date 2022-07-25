d3782734fb25de43910d031fbab2b144c126d41b
Nobby-code <norbertguda0@gmail.com>
2022-07-25 09:13:37 -0700

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@
 class Rectangle:
     """Rectangle class with setters and getters for its private variable"""
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -17,7 +17,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """setter to set new width attribute to value"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +31,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter to set height attribute to width"""
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")

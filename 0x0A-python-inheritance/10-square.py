57a96e4ee3b467c214353cf12a6e6fafe3a8696c
Nobby-code <norbertguda0@gmail.com>
2022-08-01 16:26:49 -0700

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-
+"""Sequare class importation"""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
+    """
+    Square class inherits from Rectangle
+    class
+    """
+
     def __init__(self, size):
+        """
+        init constructor
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
+    
     def area(self):
+        """
+        area() method implementation
+        """
         return self.__size * self.__size

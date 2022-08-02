9a2c54693150e5ddf129e430001d5e9032199d4b
Nobby-code <norbertguda0@gmail.com>
2022-08-01 17:22:03 -0700

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
-
 """parent class"""
+
+
 class BaseGeometry:
-    
+
     """area not implemented"""
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     """validator"""
     def integer_validator(self, name, value):
         if type(value) is not int:
@@ -15,7 +15,10 @@ class BaseGeometry:
         if value < 1:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 """sub class"""
+
+
 class Rectangle(BaseGeometry):
     """__init__ for validation - to validate height and width"""
     def __init__(self, width, height):
@@ -27,7 +30,7 @@ class Rectangle(BaseGeometry):
     """calculating the area"""
     def area(self):
         return self.__width * self.__height
-    
+
     """printing the string"""
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)

06da7fa6bcde1e870ed3c24e069e9c2de751f1e5
Nobby-code <norbertguda0@gmail.com>
2022-08-02 10:35:43 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Reading file"""
+
+
+def read_file(filename=""):
+    """
+    def read_file(filename=''):
+        function to read file
+        Args: filename is the file to read
+    The file is empty by default
+    """
+    with open(filename, encoding='utf-8') as f:
+        print(f.read())

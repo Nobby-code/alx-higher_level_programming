39895ae3f64a963700d5318c5485f150208c34cc
Nobby-code <norbertguda0@gmail.com>
2022-08-01 17:27:50 -0700

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,7 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """
-    Square class that inherites from 
+    Square class that inherites from
     Rectangle class
     """
     def __init__(self, size):
@@ -16,12 +16,13 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(self.__size, self.__size)
+
     def area(self):
         """
         area() method implementation
         """
         return self.__size * self.__size
-    
+
     def __str__(self):
         """
         Inbuils __str__ to print string format

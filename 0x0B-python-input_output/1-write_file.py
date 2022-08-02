c52b7977c6f4460f7ce7185d4f331ecaf0472ac4
Nobby-code <norbertguda0@gmail.com>
2022-08-02 11:55:21 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""module to write file"""
+
 
 def write_file(filename="", text=""):
+    """
+    Writes a text file in UTF format and returns the
+    number of characters written
+    """
     with open(filename, mode='w', encoding='utf-8') as f:
         return f.write(text)

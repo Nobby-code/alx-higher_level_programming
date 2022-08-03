7f80c79bc0d62daecb7183dfbe13543352706ca1
Nobby-code <norbertguda0@gmail.com>
2022-08-03 16:12:39 -0700

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/101-stats.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""
+reading from stdin line by line
+"""
+
+
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 """Pascal triangle function"""
 
+
 def pascal_triangle(n):
+    """
+    Returning integer representation of pascal triangle
+    """
     my_list = []
     if n <= 0:
         return my_list
-    for i in range(n):
 
+    pascal_triangle = [[1]]
+
+    for current_row in range(1, n):
+        row = [1]
+        previous_row = pascal_triangle[current_row - 1]
+
+        for element in range(1, current_row):
+            row.append(previous_row[element] + previous_row[element - 1])
+        row.append(1)
+        pascal_triangle.append(row)
+    return pascal_triangle

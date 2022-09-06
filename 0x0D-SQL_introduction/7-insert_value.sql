65408daa9e4e2202a8a5c6e766b4e1f3a54763a1
Nobby-code <norbertguda0@gmail.com>
2022-09-06 16:25:34 -0700

    Insert values

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,7 +1,4 @@
 -- Insert a new row into he table
 USE hbtn_0c_0;
-
-INSERT INTO first_table(id, name) VALUES(
-	89,
-	'Best School'
-);
+INSERT INTO first_table(id, name) 
+VALUES(89, 'Best School');

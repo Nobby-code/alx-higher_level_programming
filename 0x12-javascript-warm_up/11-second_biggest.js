377e0847372523bb446f4a224c9e8d020dbc6cdd
Nobby-code <norbertguda0@gmail.com>
2022-10-03 15:01:13 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -10,8 +10,6 @@ if (process.argv.length <= 3) {
 } 
 */
 
-#!/usr/bin/node
-// script that searches the second biggest integer in the list of arguments
 const mySol = process.argv;
 if (mySol.length <= 3) {
   console.log(0);

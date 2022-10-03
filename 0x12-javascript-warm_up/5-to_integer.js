9caaca1f5e21b82d23937a12846a71cbed5bf3b5
Nobby-code <norbertguda0@gmail.com>
2022-10-03 14:46:03 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 // Script convert first argument to number if convertable
 
 if (isNaN(process.argv[2])) {
-  console.log('Not a Number');
+  console.log('Not a number');
 } else {
   console.log(`My number: ${Number(process.argv[2])}`);
 }

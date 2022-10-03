796f6e5f15bad1bf1d0a1931550be2d874b9d212
Nobby-code <norbertguda0@gmail.com>
2022-10-03 14:52:04 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 
 // print 3 lines of array of strings using a loop
 
-const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);

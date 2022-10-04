1cc1d633a771eb2f82127143f2d8225f9f951559
Nobby-code <norbertguda0@gmail.com>
2022-10-04 11:46:11 -0700

    conditional constructure

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+// Rectangle class with a constructor having 2 arguments
+// if w or h is 0, create an empty object
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+
+    }
+  }
+}
+
+module.exports = Rectangle;

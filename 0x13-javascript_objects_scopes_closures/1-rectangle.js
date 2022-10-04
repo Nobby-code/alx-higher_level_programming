56fd48c764f4792281bbeac9fc1d197130fd61e6
Nobby-code <norbertguda0@gmail.com>
2022-10-04 11:24:54 -0700

    class with a cobnstructor

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Rectangle class with a constructor having 2 arguments
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;

fb0ef8a4451e869da8d5b6aca22d0681a2a26d4c
Nobby-code <norbertguda0@gmail.com>
2022-10-04 12:12:41 -0700

    Adding a method in a class

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// Rectangle class with a constructor having 2 arguments
+// if w or h is 0, create an empty object
+// adding a method called print()
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;

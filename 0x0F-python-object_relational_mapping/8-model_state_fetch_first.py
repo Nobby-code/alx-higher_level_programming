7c4457db491f16b3e279e9810a1b92030c5ef1ab
Nobby-code <norbertguda0@gmail.com>
2022-10-08 03:38:11 -0700

    Printing first State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+Script that lists the first State object from the database hbtn_0e_6_usa:
+    Has 3 arguments:
+        mysql username, password, database name
+    Uses module SQLAlchemy
+    Imports State and Base from model_state
+    Connects to mysql server on localhost at port 3306
+    if no object, print nothing
+"""
+
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    # create a configures "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # create a session
+    session = Session()
+
+    statement = session.query(State).first()
+    if statement:
+        print("{:d}: {:s}".format(statement.id, statement.name))
+    else:
+        print("Nothing")
+    session.close()

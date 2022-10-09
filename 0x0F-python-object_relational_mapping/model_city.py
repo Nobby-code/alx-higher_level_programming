3d83bce5fce5dd1a33fce1d562fad1dba3c6bca1
Nobby-code <norbertguda0@gmail.com>
2022-10-09 12:49:05 -0700

    model_city.py fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,7 +8,6 @@ Class definition of City that inherits from Base -> declarative
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-#Base = declarative_base()
 from model_state import Base, State
 """ 
 Instance Base -> City inherits from

e41b1e7aa3df884a431735fac81da65ca49f9498
Nobby-code <norbertguda0@gmail.com>
2022-10-09 14:21:19 -0700

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,8 +7,6 @@ Class definition of State that inherits from Base -> declarative
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-#from model_state import Base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 """ Instance """

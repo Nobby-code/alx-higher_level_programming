e9672ceb6d84ea0e1caa3712f7056a0d7a25a37e
Nobby-code <norbertguda0@gmail.com>
2022-10-23 14:12:38 -0700

    10-my_github.py - final fix

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -14,10 +14,10 @@ import sys
 
 if __name__ == "__main__":
 
-    name= str(sys.argv[1])
+    name = str(sys.argv[1])
     pswd = str(sys.argv[2])
-    r = requests.get("https://api.github.com/user",
-                          auth=(HTTPBasicAuth(name, pswd)))
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(HTTPBasicAuth(name, pswd)))
 
     try:
         data = r.json()

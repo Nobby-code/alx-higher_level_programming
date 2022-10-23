508154c350b39795fa39d4d0973972885f4a239a
Nobby-code <norbertguda0@gmail.com>
2022-10-23 05:47:21 -0700

    python script that fetched a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import urllib.request
+'''Python script that fetches a url'''
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('UTF-8')))

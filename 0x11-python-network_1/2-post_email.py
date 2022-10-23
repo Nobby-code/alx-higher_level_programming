4deebb59e1ecc328a1f894f36b6d588f68f7c9fc
Nobby-code <norbertguda0@gmail.com>
2022-10-23 08:55:29 -0700

    POST request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
-'''POST request'''
-
+'''POST request with email as a parameter'''
+import urllib.request
+import urllib.parse
+import sys
 
 
 if __name__ == "__main__":
-    import urllib.parse as parse
-    import urllib.request as request
-    from sys import argv
-    values = {'email': argv[2]}
-    data = parse.urlencode(values).encode('utf-8')
-    req = request.Request(argv[1], data)
-    with request.urlopen(req) as r:
-        print(r.read().decode('utf-8'))
+    params = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(params).encode("UTF-8")
+    req = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(req) as resp:
+        html = resp.read()
+        print(html.decode('UTF-8'))

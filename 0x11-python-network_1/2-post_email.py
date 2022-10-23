efa3ba30e279381861f8bfccfd7c085e72413e8b
Nobby-code <norbertguda0@gmail.com>
2022-10-23 08:29:25 -0700

    POST request with a parameter fixed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,14 +2,15 @@
 
 '''POST request with email as a parameter'''
 import urllib.request
+import urllib.parse
 import sys
 
 
 if __name__ == "__main__":
-    url = argv[1]
-    params = {'email': argv[2]}
-    data = urllib.parse.urlencode( params ).encode('UTF-8')
-    req = urllib.request.Request(argv[1], data)
+    url = sys.argv[1]
+    params = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(params).encode('UTF-8')
+    req = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(url, data) as resp:
         html = resp.read()
         print(html.decode('UTF-8'))
